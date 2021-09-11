3b5d39bb5962c8cf0321be15012819f96dfc0b6e
fabio823 <fmoraes05@gmail.com>
2021-09-10 21:10:59 -0300

    colocado o import do about e do contact. Também atualizado o gitignore

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,24 @@
+from typing import ContextManager
 from django.http import HttpResponse
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title" : "Página Principal"
+        "content" : "Bem-vindo à Página Principal..."
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title" : "Página Sobre"
+        "content" : "Bem-vindo à Página Sobre!!!"
+    }
+    return render(request, "about/view.html", context)
+
+def contact_page(request):
+    context = {
+        "title" : "Página Contato"
+        "content" : "Bem-vindo à Página Contato###"
+    }
+    return render(request, "contact/view.html", context)
