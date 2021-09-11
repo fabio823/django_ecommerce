b42f5479fa349ec036a4bb2baff1145e812aff97
fabio823 <fmoraes05@gmail.com>
2021-09-10 21:37:28 -0300

    Criado o conteudo para view.html no about e no contact. Atualizado o home_page.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        "title" : "Página Principal"
+        "title" : "Página Principal",
         "content" : "Bem-vindo à Página Principal..."
     }
     return render(request, "home_page.html", context)
 
 def about_page(request):
     context = {
-        "title" : "Página Sobre"
+        "title" : "Página Sobre",
         "content" : "Bem-vindo à Página Sobre!!!"
     }
     return render(request, "about/view.html", context)
 
 def contact_page(request):
     context = {
-        "title" : "Página Contato"
+        "title" : "Página Contato",
         "content" : "Bem-vindo à Página Contato###"
     }
     return render(request, "contact/view.html", context)
