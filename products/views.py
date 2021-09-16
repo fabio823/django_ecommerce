9a5a93cc4ba93edd76c3c2dccdb1348261b33924
fabio823 <fmoraes05@gmail.com>
2021-09-16 20:43:50 -0300

    Atualizado e_commerce/urls.py, products/views.py e criado products/detail.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,8 @@
+from typing import ContextManager
 from django.db.models import query
 from django.db.models.query import QuerySet
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
@@ -16,6 +17,16 @@ class ProductListView(ListView):
     #    print(context)
     #    return context
 
+class ProductDetailView(DetailView):
+    #Traz todos os produtos do banco de dados
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context =  super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
 #FBVs - Function Based Views
 def product_list_view(request):
     queryset = Product.objects.all()
@@ -23,3 +34,17 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    print(args)
+    print('-'*30)
+    print(kwargs)
+    print('-'*30)
+    #queryset = Product.objects.all()
+    #instance = Product.objects.get(pk=pk) #Pega o ID do objeto
+    instance = get_object_or_404(Product, pk=pk)
+    context = {
+        #'object_list': queryset
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
